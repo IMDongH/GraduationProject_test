27a03aea1268830b095c4d4c5c7e3e0b513b8afe
임동혁 <>
2022-08-15 16:10:04 +0900

    걸음수 체크 및 위치 데이터 확인

---
+++ app/src/main/java/com/example/graduationprojecttest/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.graduationprojecttest;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView loacationText;
+    private TextView walkingText;
+    SensorManager sensorManager;
+    Sensor stepCountSensor;
+    // 현재 걸음 수
+    int currentSteps = 0;
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loacationText = (TextView)findViewById(R.id.loactionText);
+        walkingText = (TextView)findViewById(R.id.walkingText);
+
+// 활동 퍼미션 체크
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        // 걸음 센서 연결
+        // * 옵션
+        // - TYPE_STEP_DETECTOR:  리턴 값이 무조건 1, 앱이 종료되면 다시 0부터 시작
+        // - TYPE_STEP_COUNTER : 앱 종료와 관계없이 계속 기존의 값을 가지고 있다가 1씩 증가한 값을 리턴
+        //
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        startLocationService();
+    }
+
+    public void startLocationService(){
+
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if ( Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions( MainActivity.this, new String[] {
+                        android.Manifest.permission.ACCESS_FINE_LOCATION}, 0 );
+            }
+            else{
+                // 가장최근 위치정보 가져오기
+                Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(location != null) {
+                    String provider = location.getProvider();
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    double altitude = location.getAltitude();
+
+                    loacationText.setText("위치정보 : " + provider + "\n" +
+                            "위도 : " + longitude + "\n" +
+                            "경도 : " + latitude + "\n" +
+                            "고도  : " + altitude);
+                }
+
+                // 위치정보를 원하는 시간, 거리마다 갱신해준다.
+                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        10,
+                        0,
+                        gpsLocationListener);
+                manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        10,
+                        0,
+                        gpsLocationListener);
+            }
+    }
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // 위치 리스너는 위치정보를 전달할 때 호출되므로 onLocationChanged()메소드 안에 위지청보를 처리를 작업을 구현 해야합니다.
+            String provider = location.getProvider();  // 위치정보
+            double longitude = location.getLongitude(); // 위도
+            double latitude = location.getLatitude(); // 경도
+            double altitude = location.getAltitude(); // 고도
+            loacationText.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude + "\n" + "고도 : " + altitude);
+        } public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        } public void onProviderEnabled(String provider) {
+
+        } public void onProviderDisabled(String provider) {
+
+        }
+    };
+    public void onStart() {
+        super.onStart();
+        if(stepCountSensor !=null) {
+            // 센서 속도 설정
+            // * 옵션
+            // - SENSOR_DELAY_NORMAL: 20,000 초 딜레이
+            // - SENSOR_DELAY_UI: 6,000 초 딜레이
+            // - SENSOR_DELAY_GAME: 20,000 초 딜레이
+            // - SENSOR_DELAY_FASTEST: 딜레이 없음
+            //
+            sensorManager.registerListener(this,stepCountSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // 걸음 센서 이벤트 발생시
+        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
+
+            if(event.values[0]==1.0f){
+                // 센서 이벤트가 발생할때 마다 걸음수 증가
+                currentSteps++;
+                walkingText.setText(String.valueOf(currentSteps));
+            }
+
+        }
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
